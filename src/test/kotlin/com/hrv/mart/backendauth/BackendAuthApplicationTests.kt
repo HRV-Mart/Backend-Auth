31e1c5e41a74d73ca642a6ea6a54f3719f0e570f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-23 09:36:30 +0530

    Created empty spring project

---
+++ src/test/kotlin/com/hrv/mart/backendauth/BackendAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.backendauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
