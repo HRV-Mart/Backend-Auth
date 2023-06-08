6a2fca6e3d475c9bcd8a4814659bb7df4ffd7ad4
Harsh3305 <harshverma3305@gmail.com>
2023-06-08 16:26:46 +0530

    Change auth service

--- src/main/kotlin/com/hrv/mart/backendauth/service/AuthService.kt
+++ src/main/kotlin/com/hrv/mart/backendauth/service/AuthService.kt
@@ -1,77 +1,39 @@
 package com.hrv.mart.backendauth.service
 
-import com.hrv.mart.backendauth.model.Auth
+import com.hrv.mart.backendauth.model.UserType
 import com.hrv.mart.backendauth.repository.AuthRepository
 import com.hrv.mart.backendauth.repository.KafkaRepository
-import com.hrv.mart.userlibrary.model.User
+import io.appwrite.exceptions.AppwriteException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
 
 @Service
 class AuthService (
     @Autowired
-    private val authRepository: AuthRepository,
+    private val kafkaRepository: KafkaRepository,
     @Autowired
-    private val kafkaRepository: KafkaRepository
+    private val authRepository: AuthRepository
 )
 {
-    fun login(auth: Auth, response: ServerHttpResponse) =
-        authRepository.existsAuthByEmailAndHashedPassword(auth.email, auth.hashedPassword)
-            .flatMap { exist ->
-                if (exist) {
-                    response.statusCode = HttpStatus.OK
-                    return@flatMap Mono.just("Login Successfully")
-                }
-                else {
-                    response.statusCode = HttpStatus.NOT_FOUND
-                    return@flatMap Mono.just("Auth Not Found")
-                }
-            }
-    fun signUp(auth: Auth, user: User, response: ServerHttpResponse): Mono<String> =
-        authRepository.insert(auth)
-            .flatMap {
-                response.statusCode = HttpStatus.OK
-                kafkaRepository
-                    .createUser(user)
-                    .then(
-                        Mono.just("Signup Successfully")
-                    )
-            }
-            .onErrorResume {
-                response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
-                Mono.just("Auth already exist")
-            }
-    fun updatePassword(auth: Auth, response: ServerHttpResponse) =
-        authRepository.existsById(auth.email)
-            .flatMap {
-                if (it) {
-                    response.statusCode = HttpStatus.OK
-                    authRepository.save(auth)
-                        .then(Mono.just("Password Updated Successfully"))
-                }
-                else {
-                    response.statusCode = HttpStatus.NOT_FOUND
-                    Mono.just("Auth Not Found")
-                }
-            }
-    fun deleteAuth(emailId: String, response: ServerHttpResponse) =
-        authRepository.existsById(emailId)
-            .flatMap { exist ->
-                response.statusCode = HttpStatus.OK
-                if (exist) {
-                    kafkaRepository
-                        .deleteUser(emailId)
-                        .then (
-                            authRepository.deleteById(emailId)
-                                .then(Mono.just("Auth Deleted Successfully"))
-                        )
-                }
-                else {
-                    response.statusCode = HttpStatus.NOT_FOUND
-                    Mono.just("Auth Not Found")
-                }
-            }
+    suspend fun clientRequest(
+        jwt: String,
+        userType: UserType,
+        response: ServerHttpResponse
+    ): String {
+        println(userType)
+        return try {
+            val auth = authRepository.getAuthAccount(jwt)
+            kafkaRepository
+                .createUser(auth.toUser())
+                .block()
+            response.statusCode = HttpStatus.OK
+            "Login Successful"
+
+        } catch (_: AppwriteException) {
+            response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+            "Unable to login"
+        }
+    }
 }
