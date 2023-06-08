f628ac2257f16d4b99d05ff36fc12502b70d5bc8
Harsh3305 <harshverma3305@gmail.com>
2023-06-08 16:37:09 +0530

    Minor change

--- src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
+++ src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
@@ -2,6 +2,7 @@ package com.hrv.mart.backendauth.controller
 
 import com.hrv.mart.backendauth.model.UserType
 import com.hrv.mart.backendauth.service.AuthService
+import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,10 +21,24 @@ class AuthController (
         @RequestParam jwt: Optional<String>,
         @RequestParam userType: Optional<UserType>,
         response: ServerHttpResponse
-    ) =
-        authService.clientRequest(
-            jwt = jwt.get(),
-            userType.get(),
-            response
-        )
+    ): String {
+        if (jwt.isEmpty) {
+            response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+            return "Please send JWT"
+        }
+        else if (userType.isEmpty) {
+            return authService.clientRequest(
+                jwt = jwt.get(),
+                UserType.USER,
+                response
+            )
+        }
+        else {
+            return authService.clientRequest(
+                jwt = jwt.get(),
+                userType.get(),
+                response
+            )
+        }
+    }
 }
