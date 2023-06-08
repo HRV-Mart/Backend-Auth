d5f8688e1406fb382dfbe312a11cd00dab9d5925
Harsh3305 <harshverma3305@gmail.com>
2023-06-08 16:27:21 +0530

    Remove previous auth routes and add new endpoint

--- src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
+++ src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
@@ -1,39 +1,29 @@
 package com.hrv.mart.backendauth.controller
 
-import com.hrv.mart.backendauth.model.Auth
-import com.hrv.mart.backendauth.model.UserDetail
+import com.hrv.mart.backendauth.model.UserType
 import com.hrv.mart.backendauth.service.AuthService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.server.reactive.ServerHttpResponse
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 @RestController
 @RequestMapping("/auth")
 class AuthController (
-    @Autowired
     private val authService: AuthService
 )
 {
-    @PostMapping("/signup")
-    fun signUp(@RequestBody userDetail: UserDetail, response: ServerHttpResponse) =
-        authService.signUp(
-            userDetail.authDetail,
-            userDetail.userDetail,
+    @GetMapping
+    suspend fun getInfoFromJWT(
+        @RequestParam jwt: Optional<String>,
+        @RequestParam userType: Optional<UserType>,
+        response: ServerHttpResponse
+    ) =
+        authService.clientRequest(
+            jwt = jwt.get(),
+            userType.get(),
             response
         )
-    @PostMapping("/login")
-    fun login(@RequestBody auth: Auth, response: ServerHttpResponse) =
-        authService.login(auth, response)
-    @PutMapping
-    fun updatePassword(@RequestBody auth: Auth, response: ServerHttpResponse) =
-        authService.updatePassword(auth, response)
-    @DeleteMapping("/{emailId}")
-    fun deleteAuth(@PathVariable emailId: String, response: ServerHttpResponse) =
-        authService.deleteAuth(emailId, response)
 }
