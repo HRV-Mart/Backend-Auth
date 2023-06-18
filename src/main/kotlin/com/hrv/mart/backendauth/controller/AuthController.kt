443a2fba9d0f02c75bd788683aafaebac52c002a
Harsh3305 <harshverma3305@gmail.com>
2023-06-18 09:46:38 +0530

    Change endpoint from getMapping to postmapping

--- src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
+++ src/main/kotlin/com/hrv/mart/backendauth/controller/AuthController.kt
@@ -1,18 +1,12 @@
 package com.hrv.mart.backendauth.controller
 
-import com.hrv.mart.authlibrary.model.Auth
-import com.hrv.mart.authlibrary.model.UserType
+import com.hrv.mart.authlibrary.model.AuthRequest
 import com.hrv.mart.backendauth.service.AuthService
-import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
-import java.util.Optional
-import kotlin.jvm.optionals.getOrDefault
-import kotlin.jvm.optionals.getOrElse
 
 @RestController
 @RequestMapping("/auth")
@@ -20,22 +14,15 @@ class AuthController (
     private val authService: AuthService
 )
 {
-    @OptIn(ExperimentalStdlibApi::class)
-    @GetMapping
+    @PostMapping
     fun getInfoFromJWT(
-        @RequestParam jwt: Optional<String>,
-        @RequestParam userType: Optional<UserType>,
+        @RequestBody authRequest: AuthRequest,
         response: ServerHttpResponse
-    ): Mono<Auth> {
-        return if (jwt.isEmpty) {
-            response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
-            Mono.empty()
-        } else {
-            authService.clientRequest(
-                jwt = jwt.get(),
-                userType = userType.getOrDefault(UserType.USER),
-                response
+    ) =
+        authService
+            .clientRequest(
+                jwt = authRequest.jwt,
+                userType = authRequest.userType,
+                response = response
             )
-        }
-    }
 }
